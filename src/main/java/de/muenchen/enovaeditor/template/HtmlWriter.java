d1ade4a6bc517050cd2317938076c57dce448c06
mohamad.daaeboul <mohamad.daaeboul@muenchen.de>
2026-09-22 10:34:00 +0200

    Add HTML generation for transformed answer

--- src/main/java/de/muenchen/enovaeditor/template/HtmlWriter.java
+++ src/main/java/de/muenchen/enovaeditor/template/HtmlWriter.java
@@ -1,17 +1,13 @@
 package de.muenchen.enovaeditor.template;
 
-import de.muenchen.enovaeditor.util.OutputPathUtil;
-
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-public class HtmlOutputWriter {
+public class HtmlWriter {
 
-    public Path write(String html, Path inputXmlPath) throws IOException {
-
-        Path outputPath = OutputPathUtil.createOutputPath(inputXmlPath, ".htm");
+    public Path write(String html, Path outputPath) throws IOException {
 
         Files.writeString(outputPath, html, StandardCharsets.UTF_8);
 
